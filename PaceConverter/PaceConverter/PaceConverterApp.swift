95fe907874b58aad0c2d40a5ccdd01c2a1e0630e
Martin Rist <mlr-github@martinrist.com>
2021-06-14 13:30:40 +0100

    Add blank Xcode project

---
+++ PaceConverter/PaceConverter/PaceConverterApp.swift
@@ -0,0 +1,17 @@
+//
+//  PaceConverterApp.swift
+//  PaceConverter
+//
+//  Created by Martin Rist on 14/06/2021.
+//
+
+import SwiftUI
+
+@main
+struct PaceConverterApp: App {
+  var body: some Scene {
+    WindowGroup {
+      ContentView()
+    }
+  }
+}
